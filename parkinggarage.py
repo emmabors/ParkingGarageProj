4fe387839c8e4df78b95f6ffbbb8a038bb39e376
Emma Borsavage <emma_borsavage@aol.com>
2023-02-13 10:59:54 -0700

    emma driver

--- parkinggarage.py
+++ parkinggarage.py
@@ -5,14 +5,16 @@ class Garage():
         self.parking = []
         self.current_ticket = {}
 
-    def space_display(self):
-        print(f'\nSpaces open: {len(self.tickets)}')
-
-    def create_garage(self):       
-        self.tickets = [i for i in range(self.num_of_tickets)]
+    def create_garage(self):
+        self.tickets = [i + 1 for i in range(self.num_of_tickets)]
         spaces_open = len(self.tickets) - len(self.parking)
         for i in range(spaces_open):
             self.parking.append('____')
+
+    def space_display(self):
+        print(f'\nSpaces open: {len(self.tickets)}')
+
+    def display_garage(self):       
         print('              / \ ')
         print('__________----|_|_______')
         parked = ''
@@ -23,50 +25,68 @@ class Garage():
                 parked += f'|_{x}     |'[::-1] +  '\n'
         print(parked)
 
-    # def take_ticket(self):
-    #     bike = '.-._'
-    #     truck = ':-:='
-    #     car = ':-:_'
-    #     vehicle = int(input('What kind of vehicle do you have? | 1: Car | 2: Truck | 3: Bike |'))
-    #     parking_spot = int(input('What Spot would you like to take?'))
-    #     if  self.parking[parking_spot] == '____':
-    #         if vehicle == 1:
-    #             #pop and splice
-    #         elif vehicle == 2:
-    #             #pop and splice
-    #         elif vehicle == 3:
-    #             #pop and splice
-    #         else:
-    #                 print('Please enter a valid number')
-    #     else:
-    #         print('Parking spot is already taken')
-    #     print(f'{parking_spot} ticket has been administered - find open spot')
+    def take_ticket(self):
+        bike = '.-._'
+        truck = ':-:='
+        car = ':-:_'
+        vehicle = int(input('What kind of vehicle do you have? | 1: Car | 2: Truck | 3: Bike |'))
+        parking_spot = int(input('What Spot would you like to take?'))
+        parking_spot -= 1
+        if  self.parking[parking_spot] == '____':
+            if vehicle == 1:
+                self.tickets.pop(parking_spot)
+                self.parking.pop(parking_spot)
+                self.parking[parking_spot:parking_spot] = [car]
+            elif vehicle == 2:
+                self.tickets.pop(parking_spot)
+                self.parking.pop(parking_spot)
+                self.parking[parking_spot:parking_spot] = [truck]
+            elif vehicle == 3:
+                self.tickets.pop(parking_spot)
+                self.parking.pop(parking_spot)
+                self.parking[parking_spot:parking_spot] = [bike]
+            else:
+                print('Please enter a valid number')
+        else:
+            print('Parking spot is already taken')
+        print(f'{parking_spot} ticket has been administered - find open spot')
 
     def pay_for_parking(self):
-        amount = input('Please pay $15(type 15 for $15)')
+        amount = int(input('Please pay $15(type 15 for $15)'))
         if amount == 15:
             self.current_ticket['paid'] = True
         else: 
             print('Please pay the fixed amount')
-        self.pay_for_parking()       
+            self.pay_for_parking()       
     
-    # def leave_garage(self):
-        #input for what parking_spot they are in
-        #if  self.parking[parking_spot] != '____':
-        # if self.current_ticket.values() == True:
-        #     print('Thank you, have a nice day!')
-        #     self.tickets.append(ticket)
-        # if self.current_ticket.values() == False:
-        #     self.pay_for_parking()
-        #pop and splice
+    def leave_garage(self):
+        parking_spot = int(input('What spot were you in?: '))
+        parking_spot -= 1
+        if  self.parking[parking_spot] != '____':
+            if self.current_ticket['paid'] == True:
+                self.parking.pop(parking_spot)
+                self.parking[parking_spot:parking_spot] = ['____']
+                self.tickets[parking_spot:parking_spot] = [parking_spot + 1] 
+            else:
+                self.pay_for_parking()
+        else:
+            print('space is already empty')
+        print('Thank you, have a nice day!')
+                   
 
 
     def runner(self):
         self.create_garage()
-        self.take_ticket()
-        self.create_garage()
-        self.space_display()
         print(self.tickets)
+        self.display_garage()
+        self.take_ticket()
+        self.space_display()
+        self.display_garage()
+        self.pay_for_parking()
+        self.leave_garage()
+        print(self.tickets)
+        self.display_garage()
+        
 
 
 luke = Garage(20)
